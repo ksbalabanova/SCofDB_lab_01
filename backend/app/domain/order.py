12f950ac1dc272bff857ac340dc0d0969b3af7a2
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2026-03-07 10:10:23 +0300

    Update order.py

--- backend/app/domain/order.py
+++ backend/app/domain/order.py
@@ -4,7 +4,7 @@ import uuid
 from datetime import datetime
 from decimal import Decimal
 from enum import Enum
-from typing import List
+from typing import List, Optional
 
 from .exceptions import (
     OrderAlreadyPaidError,
@@ -14,26 +14,51 @@ from .exceptions import (
     InvalidAmountError,
 )
 
+from dataclasses import dataclass, field
+
 
 # TODO: Реализовать OrderStatus (str, Enum)
 # Значения: CREATED, PAID, CANCELLED, SHIPPED, COMPLETED
 class OrderStatus(str, Enum):
-    pass
+    CREATED = 'CREATED'
+    PAID = 'PAID'
+    CANCELLED = 'CANCELLED'
+    SHIPPED = 'SHIPPED'
+    COMPLETED = 'COMPLETED'
 
 
 # TODO: Реализовать OrderItem (dataclass)
 # Поля: product_name, price, quantity, id, order_id
 # Свойство: subtotal (price * quantity)
 # Валидация: quantity > 0, price >= 0
+@dataclass
 class OrderItem:
-    pass
+    product_name: str
+    price: Decimal
+    quantity: int
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
+    order_id: Optional[uuid.UUID] = None
+
+    @property
+    def subtotal(self) -> Decimal:
+        return self.price * self.quantity
+
+    def __post_init__(self):
+        if self.quantity <= 0:
+            raise InvalidQuantityError(self.quantity)
+        if self.price < 0:
+            raise InvalidPriceError(self.price)
+    
 
 
 # TODO: Реализовать OrderStatusChange (dataclass)
 # Поля: order_id, status, changed_at, id
+@dataclass
 class OrderStatusChange:
-    pass
-
+    order_id: uuid.UUID
+    status: OrderStatus
+    changed_at: datetime = field(default_factory=datetime.utcnow)
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
 
 # TODO: Реализовать Order (dataclass)
 # Поля: user_id, id, status, total_amount, created_at, items, status_history
@@ -43,5 +68,51 @@ class OrderStatusChange:
 #   - cancel() -> None
 #   - ship() -> None
 #   - complete() -> None
+@dataclass
 class Order:
-    pass
+    user_id: uuid.UUID
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
+    status: OrderStatus = OrderStatus.CREATED
+    total_amount: Decimal = Decimal("0")
+    created_at: datetime = field(default_factory=datetime.utcnow)
+    items: List[OrderItem] = field(default_factory=list)
+    status_history: List[OrderStatusChange] = field(default_factory=list)
+
+    def add_item(self, product_name: str, price: Decimal, quantity: int) -> OrderItem:
+        if self.status == OrderStatus.CANCELLED:
+            raise OrderCancelledError(self.id)
+        order_item = OrderItem(
+            product_name = product_name,
+            price = price,
+            quantity = quantity,
+            order_id= self.id
+        )
+        self.items.append(order_item)
+        self.total_amount += order_item.subtotal
+        return order_item
+
+    def pay(self) -> None:
+        if self.status == OrderStatus.PAID:
+            raise OrderAlreadyPaidError(self.id)
+        if self.status == OrderStatus.CANCELLED:
+            raise OrderCancelledError(self.id)
+        self.status = OrderStatus.PAID
+        self.status_history.append(OrderStatusChange(order_id=self.id, status=OrderStatus.PAID))
+
+    def cancel(self) -> None:
+        if self.status == OrderStatus.PAID:
+            raise OrderAlreadyPaidError(self.id)
+        self.status = OrderStatus.CANCELLED
+        self.status_history.append(OrderStatusChange(order_id=self.id, status=OrderStatus.CANCELLED))
+
+    def ship(self) -> None:
+        if self.status != OrderStatus.PAID:
+            raise ValueError()
+        self.status = OrderStatus.SHIPPED
+        self.status_history.append(OrderStatusChange(order_id=self.id, status=OrderStatus.SHIPPED))
+
+    def complete(self) -> None:
+        if self.status != OrderStatus.SHIPPED:
+            raise ValueError()
+        self.status = OrderStatus.COMPLETED
+        self.status_history.append(OrderStatusChange(order_id=self.id, status=OrderStatus.COMPLETED))

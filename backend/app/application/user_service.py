4bddf1ecb6f3c34f214e142d6953439f4d5d5ef1
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2026-03-07 10:15:56 +0300

    Update user_service.py

--- backend/app/application/user_service.py
+++ backend/app/application/user_service.py
@@ -18,16 +18,29 @@ class UserService:
     # 2. Создать User
     # 3. Сохранить через repo.save()
     async def register(self, email: str, name: str = "") -> User:
-        raise NotImplementedError("TODO: Реализовать UserService.register")
+        #raise NotImplementedError("TODO: Реализовать UserService.register")
+        existing = await self.repo.find_by_email(email)
+        if existing is not None:
+            raise EmailAlreadyExistsError(email)
+        user = User(email=email, name=name)
+        await self.repo.save(user)
+        return user
 
     # TODO: Реализовать get_by_id(user_id) -> User
     async def get_by_id(self, user_id: uuid.UUID) -> User:
-        raise NotImplementedError("TODO: Реализовать UserService.get_by_id")
+        #raise NotImplementedError("TODO: Реализовать UserService.get_by_id")
+        user = await self.repo.find_by_id(user_id)
+        if user is None:
+            raise UserNotFoundError(user_id)
+        return user
+
 
     # TODO: Реализовать get_by_email(email) -> Optional[User]
     async def get_by_email(self, email: str) -> Optional[User]:
-        raise NotImplementedError("TODO: Реализовать UserService.get_by_email")
+        #raise NotImplementedError("TODO: Реализовать UserService.get_by_email")
+        return await self.repo.find_by_email(email)
 
     # TODO: Реализовать list_users() -> List[User]
     async def list_users(self) -> List[User]:
-        raise NotImplementedError("TODO: Реализовать UserService.list_users")
+        #raise NotImplementedError("TODO: Реализовать UserService.list_users")
+        return await self.repo.find_all()
